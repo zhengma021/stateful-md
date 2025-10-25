ee63b3e8a646f30935956d11df606f69f9dc3564
zhengma <zhengma60@gmail.com>
2025-10-25 21:38:27 +0800

    Make the prompt clearer

--- prompt.clj
+++ prompt.clj
@@ -1,15 +1,5 @@
 (ns prompt)
 
-(defn determine-the-user-choosed-task []
-  ;; The user can choose one task from the the give task list:
-  ;; The function should return the task name and the task arguments
-  ;; 1. s-md-visible: to make the markdown content visible, the arguments should be:
-  ;;    --file: markdown file path
-  ;;    --sharing-name: the name to share the markdown content
-  ;;    --checking-url: the url to check visibility every 1 second
-  ;;    --port: the port to run the task
-  )
-
 (defn check-s-md-visible-checking-url-argument [checking-url]
   ;; ;; All arguments should be given via command line arguments
   ;; ;;The given checking-url should be a valid url and be accessible
@@ -23,32 +13,47 @@
     ;;throw error to tell the user the checking-url is invalid and the details of the error
     false)
 
-;; ;;This task will make the markdown content visible on the given port, which mean when the user access the localhost:<port>, the markdown content will be rendered and visible
-  ;; ;;It will check the visibility of the markdown content on the given url every 1 second
-  ;; ;;If the content is not visible, it will reload the page
-  ;; ;;If the accesser to the localhost:<port> disabled the javascript, the content will not be visible
-  ;; ;;The markdown content will not be posiible or hard to copy for normal user
+  ;; ;;This task will make the markdown content visible on the given port
+  )
+
+(defn access-the-not-found-page []
+  ;; ;;This function will return a not found page
+  ;; ;;The not found page should tell the user the requested markdown content is not found
+  ;; ;;It should also provide a link to go back to the home page
   )
 
 (defn access-the-stateful-sharing-md [sharing-name]
-  ;; ;;This function will return the markdown content via the sharing-name
+  ;; ;;This function will return the markdown content of the sharing-name
   ;; ;;It should return the markdown content if the sharing-name is valid
-  ;; ;;If the sharing-name is invalid, it should return an error message
   ;; ;;The returned markdown content should be rendered on the page
   ;; ;; will check the visibility of the markdown content on the given url every 1 second
-  ;; ;;If the content is not visible, will do a reload of the page
-  ;; The "exists" only means the given sharing-name is valid as this is a LLM prompt in clojure style not code to run
+  ;; ;;If the accesser of the sharing-name targed md content disabled the javascript, the content will not be visible
+  ;; ;;The markdown content will not be posiible or hard to copy for normal user
+  ;; ;;If the content is not visible, will do a reload on the page
+
+  ;; The valid-sharing-name in below only means a condition that the sharing-name exists
   (when-not (= "valid-sharing-name" sharing-name)
     ;;return an error message included html content
-    false)
-  ;;return the the markdown content
+    (access-the-not-found-page))
+  ;;return the markdown content of the given sharing-name included html content
   true)
 
+(defn s-md-content-visible? [checking-url]
+  ;; ;;This function will check the visibility of the markdown content on the given url
+  ;; ;;It should return true if the content is visible
+  ;; ;;It should return false if the content is not visible
+  )
+
+(defn sharing-name->checking-url [sharing-name]
+  ;; ;;This function will return the checking-url for the given sharing-name
+  ;; ;;The checking-url should be in the format: http://<domain>/check-md-visible/<sharing-name>
+  )
+
 (defn when-user-access-the-md-visible-page [sharing-name]
-  ;; ;;This function will be called when the user access the md visible page
-  ;; ;; If the cheking url return visible false, it will show a message to tell the user the content is not visible
-  ;; ;;If the checking url return visible true, it will render the markdown content on the page
-  (access-the-stateful-sharing-md sharing-name))
+  (let [visible? (s-md-content-visible? (sharing-name->checking-url sharing-name))]
+    (if visible?
+      (access-the-stateful-sharing-md sharing-name)
+      (access-the-not-found-page))))
 
 (defn set-md-content-on-the-route [sharing-name]
   ;; ;;Should set up the route to access the markdown content via the sharing-name
@@ -62,6 +67,16 @@
     "s-md-visible" (run-s-md-visible-task task-args)
     (str "Unsupported: " task-name))
   (set-md-content-on-the-route (get task-args :sharing-name)))
+
+(defn determine-the-user-choosed-task []
+  ;; The user can choose one task from the the give task list:
+  ;; The function should return the task name and the task arguments
+  ;; 1. s-md-visible: to make the markdown content visible, the arguments should be:
+  ;;    --file: markdown file path
+  ;;    --sharing-name: the name to share the markdown content
+  ;;    --checking-url: the url to check visibility every 1 second
+  ;;    --port: the port to run the task
+  )
 
 (defn main []
   ;;; This should be a typescript project
